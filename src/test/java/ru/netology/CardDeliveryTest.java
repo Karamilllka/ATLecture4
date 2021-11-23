8f2e3c285f3c5b5d43f7cef11ab7505c625cff86
Ekaterina Miheeva <karamilllka@gmail.com>
2021-11-23 18:02:33 +0600

    add test

---
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -0,0 +1,28 @@
+package ru.netology;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.exist;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class CardDeliveryTest {
+    @Test
+    void shouldRegisterDeliveryCard () {
+        open("http://localhost:9999/");
+        $("[data-test-id=city] input").setValue("Екатеринбург");
+        $("[data-test-id=date] input").setValue("30.11.2021");
+        $("[data-test-id=name] input").setValue("Зотов Александр");
+        $("[data-test-id=phone] input").setValue("+79123456789");
+        $("[data-test-id=agreement]").click();
+        $(".button").click();
+        $(withText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
+
+    }
+
+}
